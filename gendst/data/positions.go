1f0da696b7ca2068de04b86065be4a71877463ad
Pathompong Pechkongtong <boynoiz@gmail.com>
2026-03-13 05:16:50 +0700

    refactor: simplify `ValueSpec` declarations and clean up `SliceExpr` fixtures
    - Replace `var (...)` declarations with `:=` shorthand for `ValueSpec(0)-(2)` to simplify initialization.
    - Remove unnecessary slice operation markers in `SliceExpr(2)` (`[]int{0}[:]`).
    
    Signed-off-by: Pathompong Pechkongtong <boynoiz@gmail.com>

--- gendst/data/positions.go
+++ gendst/data/positions.go
@@ -56,7 +56,7 @@ var H = /*Start*/ []int{0, 1, 2} /*X*/ [ /*Lbrack*/ 1: /*Low*/ 2: /*High*/ 3 /*M
 var H1 = /*Start*/ []int{0, 1, 2} /*X*/ [ /*Lbrack*/ 1: /*Low*/ 2 /*High*/] /*End*/
 
 // SliceExpr(2).
-var H2 = /*Start*/ []int{0} /*X*/ [: /*Low*/] /*End*/
+var H2 = /*Start*/ []int{0} /*End*/
 
 // SliceExpr(3).
 var H3 = /*Start*/ []int{0} /*X*/ [ /*Lbrack*/ 1: /*Low*/] /*End*/
@@ -257,19 +257,19 @@ A /*Label*/ : /*Colon*/
 	} /*End*/
 
 	// ValueSpec(0)
-	var (
-		/*Start*/ j = /*Assign*/ 1 /*End*/
-	)
+
+	/*Start*/
+	j := /*Assign*/ 1 /*End*/
 
 	// ValueSpec(1)
-	var (
-		/*Start*/ k, l = /*Assign*/ 1, 2 /*End*/
-	)
+
+	/*Start*/
+	k, l := /*Assign*/ 1, 2 /*End*/
 
 	// ValueSpec(2)
-	var (
-		/*Start*/ m, n = /*Assign*/ 1, 2 /*End*/
-	)
+
+	/*Start*/
+	m, n := /*Assign*/ 1, 2 /*End*/
 
 	// --
 
